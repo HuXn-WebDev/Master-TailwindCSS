018c5d08bde66ca559bb926cef125c5903e92449
HuXn WebDev <webdevacademy6@gmail.com>
2023-02-02 04:38:27 -0800

    Finished (Web Agency) Project 2

---
+++ Finished Code/Projects/2. Web Agency/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
